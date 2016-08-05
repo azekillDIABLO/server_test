92821d0c94ea6fc8dc94a3cbc17b2f7c8eb4300c
Miguel Isaac <mikik@hotmail.es>
2016-08-04 19:12:12 -0500

    Create init.lua

---
+++ mods/server_master/init.lua
@@ -0,0 +1,4 @@
+--server_master code
+--Mods that help to administrators to have control under players that don't agree rules!
+dofile(minetest.get_modpath("server_master").."/aban.lua")
+dofile(minetest.get_modpath("server_master").."/admin_center.lua")
