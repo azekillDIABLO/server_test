b43368527735948439797d6dc0a477b43c722b44
Miguel Isaac <mikik@hotmail.es>
2016-08-04 19:08:55 -0500

    Create init.lua

---
+++ mods/server_player/init.lua
@@ -0,0 +1,4 @@
+--server_player code
+--Mods that help players to have better experience when playing.
+dofile(minetest.get_modpath("server_player").."/protector_block.lua")
+dofile(minetest.get_modpath("server_player").."/player_center.lua")
