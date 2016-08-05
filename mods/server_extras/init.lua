f77e425a26654f4e8536af81c5bb0b4dfb843018
Miguel Isaac <mikik@hotmail.es>
2016-08-04 19:40:35 -0500

    Create init.lua

---
+++ mods/server_extras/init.lua
@@ -0,0 +1,4 @@
+--server-extras
+--This contains extras likely skins to add some stuff to the server
+dofile(minetest.get_modpath("server_extras").."/skins.lua")
+dofile(minetest.get_modpath("server_extras").."/mail.lua")
